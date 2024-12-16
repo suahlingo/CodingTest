093e78da6ff0094b3f31666d6f7be2253bc7091d
soapcookie <sgua0720@gmail.com>
2024-12-16 21:48:29 +0900

    [solve]:1000_sprout

---
+++ AplusB_1000.java
@@ -0,0 +1,26 @@
+package CodingTest;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class AplusB_1000 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int sum = A+B;
+
+        bw.write(String.valueOf(sum)); // bw는 integer를 못쓰기 때문에 String을 변환해서 값 출력
+
+        bw.flush();
+
+        bw.close();
+
+    }
+
+}
