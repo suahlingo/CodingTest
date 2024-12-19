2a3d9db148d7e621f657aa986a354b4ad648a078
suahlingo <sgua0720@gmail>
2024-12-19 13:33:16 +0900

    [attempt] : 1330_sprout

---
+++ sua/CompareTwoNumbers_1330.java
@@ -0,0 +1,13 @@
+public class CompareTwoNumbers {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+
+    }
+}
