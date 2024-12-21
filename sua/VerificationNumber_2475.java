3c0f33f16302c801bfaac750458121e36fdafb1c
soapcookie <sgua0720@gmail.com>
2024-12-21 20:44:46 +0900

    [solve]:2475_sprout

---
+++ sua/VerificationNumber_2475.java
@@ -0,0 +1,29 @@
+package CodingTest.sua;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class VerificationNumber_2475 {
+    public static void main(String[] args)throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+
+        int res = (A*A + B*B + C*C + D*D + E*E)%10;
+
+        bw.write(String.valueOf(res));
+        bw.flush();
+        bw.close();
+
+
+
+    }
+}
