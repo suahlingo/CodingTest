adefcba693a9f810298c5dca8f848907d7e482c0
soapcookie <sgua0720@gmail.com>
2024-12-21 21:24:38 +0900

    [review]:2741_sprout

--- sua/VerificationNumber_2475.java
+++ sua/VerificationNumber_2475.java
@@ -11,6 +11,7 @@ public class VerificationNumber_2475 {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        //for문으로 다시.
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
