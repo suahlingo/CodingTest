62a8b0f9571c5c33470b630ccb84ae9c16187855
soapcookie <sgua0720@gmail.com>
2024-12-21 18:08:55 +0900

    [solve]:1330_sprout

--- sua/CompareTwoNumbers_1330.java
+++ sua/CompareTwoNumbers_1330.java
@@ -1,6 +1,13 @@
-public class CompareTwoNumbers {
-    public static void main(String[] args) {
+package CodingTest.sua;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class CompareTwoNumbers_1330 {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -8,6 +15,13 @@ public class CompareTwoNumbers {
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
 
+        bw.write((A>B)?">":(A<B)?"<":"==");
+
+        bw.flush();
+
+        bw.close();
+
 
     }
+
 }
